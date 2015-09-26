7ad845f8d398014d5af2000b518572e71cc6607f
Jonathan Lameira <jlameira@gmail.com>
2015-09-26 00:59:38 -0300

    Inserção do starActivity

--- app/src/main/java/br/com/jolameira/aplicacaogcp/MainActivity.java
+++ app/src/main/java/br/com/jolameira/aplicacaogcp/MainActivity.java
@@ -1,19 +1,25 @@
 package br.com.jolameira.aplicacaogcp;
 
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends Activity implements FragmentA.Communicator {
+public class MainActivity extends Activity implements FragmentA.Communicator,FragmentManager.OnBackStackChangedListener {
 
     FragmentA f1;
     FragmentB f2;
     FragmentManager manager;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,15 @@ public class MainActivity extends Activity implements FragmentA.Communicator {
         manager = getFragmentManager();
         f1 =(FragmentA) manager.findFragmentById(R.id.fragment);
         f1.setCommunicator(this);
+        text = (TextView) findViewById(R.id.textView);
+        manager.addOnBackStackChangedListener(this);
+
+        if(savedInstanceState == null){
+            FragmentA fragmentA = new FragmentA();
+            FragmentB fragmentB = new FragmentB();
+            FragmentTransaction ft = manager.beginTransaction();
+            ft.add(R.id.fragment,fragmentA,"fragmentA");
+        }
     }
 
     @Override
@@ -30,10 +45,35 @@ public class MainActivity extends Activity implements FragmentA.Communicator {
         f2 = (FragmentB) manager.findFragmentById(R.id.fragment2);
         if(f2 != null && f2.isVisible()){
             f2.mudarDados(index);
+//            addB(f2.getView());
         }else{
 
             Intent intent = new Intent(this,AnotherActivity.class);
+            intent.putExtra("index", index);
+            startActivity(intent);
         }
 
+    }
+
+    public void addB(View v){
+        FragmentB fragmentB = new FragmentB();
+        FragmentTransaction ft = manager.beginTransaction();
+        ft.add(R.id.fragment,fragmentB,"fragmentB");
+        ft.addToBackStack("addB");
+        ft.commit();
+    }
+
+
+    @Override
+    public void onBackStackChanged() {
+        text.setText(text.getText()+"\n");
+        text.setText(text.getText() + "Texto da pilha");
+
+        int count= manager.getBackStackEntryCount();
+        for(int i = count-1 ; i>0 ;i--){
+            FragmentManager.BackStackEntry entry =  manager.getBackStackEntryAt(i);
+            text.setText(text.getText()+" "+entry.getName()+" \n");
+        }
+        text.setText(text.getText()+"\n");
     }
 }
